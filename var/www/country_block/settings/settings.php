17af72b3d0fcc967298e7ae196476b948c9bdd9c
Dominik Dorn <dominik.dorn@gmail.com>
2015-09-06 13:07:30 +0200

    settings.php file

---
+++ var/www/country_block/settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+# override these settings in your settings.php file
+
+$supportEmail = "office@lyrix.at";
+$websiteUrl = "https://www.lyrix.at/";
+$recaptchaSiteKey = "6Ldv8AYTAAAAAIhkX393tJzP_5DVebvLyL-gK-j2";
+$recaptchaSecretKey = "6Ldv8AYTAAAAAAY5nhpR9g9E_MqaR1I0f7NX2aWu";
+
+
+
